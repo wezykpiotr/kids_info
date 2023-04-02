4e2d46da736f488dafcb86975cf3dbdc5017d231
Piotr Wezyk <piotr.wezykk@gmail.com>
2023-04-02 13:27:12 +0200

    Add repository, model, data source

--- kids_info/lib/app/core/injection_container.dart
+++ kids_info/lib/app/core/injection_container.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'injection_container.config.dart';
@@ -6,3 +7,14 @@ final getIt = GetIt.instance;
 
 @InjectableInit()
 void configureDependencies() => getIt.init();
+
+@module
+abstract class RegisterModule {
+  // You can register named preemptive types like follows
+  @Named("BaseUrl")
+  String get baseUrl => 'https://json.extendsclass.com/bin/';
+
+  // url here will be injected
+  @lazySingleton
+  Dio dio(@Named('BaseUrl') String url) => Dio(BaseOptions(baseUrl: url));
+}
---
+++ kids_info/lib/domain/model/boy_model.dart
@@ -0,0 +1,21 @@
+import 'package:json_annotation/json_annotation.dart';
+
+// part 'boy_model.g.dart';
+
+@JsonSerializable()
+class BoyModel {
+  // BoyModel({
+  //   required this.id,
+  //   required this.weight,
+  //   required this.age,
+  //   required this.name,
+  // });
+  // int id;
+  // int weight;
+  // int age;
+  // String name;
+
+
+  //   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+
+}
